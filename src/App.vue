8a6f04170cfbbdf62db3728ef7f6fd6e2d1a65d4
CodeKiro <kirofiniello@outlook.it>
2024-06-18 00:42:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use '../node_modules/bootstrap/scss/bootstrap.scss';
+@use './styles/partials/mixins' as *;
+@use './styles/partials/variables' as *;
+</style>
