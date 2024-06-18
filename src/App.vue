d20d632b39ac6a09b4d6b271522f9502e9bdd671
CodeKiro <kirofiniello@outlook.it>
2024-06-18 10:12:26 +0200

    ho dato un minimo di struttura per la prima milestone

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+
 export default {
+    components:{
+        AppHeader,
+        AppMain
+    },
     data() {
-        return {
+        return 
+            {
         }
     }
 }
 </script>
 
 <template>
+  
+<AppHeader/>
+<AppMain/>
 
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+<div class="container">
+    <div class="row">
+        <div class="form-floating mb-3 d-flex">
+    <input type="text" class="form-control" id="floatingInput" placeholder="scrivi un titolo di un film">
+    <label for="floatingInput">Scrivi un titolo</label>
+    <button class="btn btn-primary">Cerca</button>
+    </div>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
